96d5c0fe618c28a4dcf2e05818bb3cb9c413eaf7
Rich Burroughs <rich@richburroughs.com>
2016-05-15 12:10:53 -0700

    making r10k deploy production environment only

--- bootstrap/master.sh
+++ bootstrap/master.sh
@@ -11,6 +11,6 @@ systemctl enable puppetserver
 
 puppet module install zack-r10k
 puppet apply -e "class { 'r10k': remote => 'https://github.com/richburroughs/puppet-control-foss' }"
-r10k deploy environment -pv
+r10k deploy environment production -pv
 
 puppet agent -t
