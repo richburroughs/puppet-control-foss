6db65511bba0df267b2b0e9edd25802dc9620742
Rich Burroughs <rich@richburroughs.com>
2016-05-15 09:52:10 -0700

    added r10k to master node

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,9 @@
 node ‘puppet*' {
   include puppetdb
   include puppetdb::master::config
+  class { 'r10k':
+      remote => 'git@github.com/richburroughs/puppet-control-foss',
+  }
 }
 
 node default {}
